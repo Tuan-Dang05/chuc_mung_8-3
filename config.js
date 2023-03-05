372bd3e1e376c1b0c9b92d38bc76b31855f79c59
Đặng Thi Mai Anh <dangthimaianh78@Tuan-Dev.local>
2023-03-05 23:50:23 +0700

    deloy

---
+++ config.js
@@ -0,0 +1,12 @@
+const nameGirl = '8/3';
+const giftUrl = 'https://www.facebook.com/tundz05';
+const eventName = 'Chúc Mừng Ngày Phụ Nữ';
+const titleCard = 'Chúc các bạn 8/3 xinh đẹp như ảnh trên mạng <3';
+const contentCard = 'chạm vào đây sẽ có điều bất ngờ nè !!';
+
+// phần dưới dành cho các bạn biết code, nếu muốn chỉnh ảnh đơn giản với base64
+// Mr-Nam http://facebook.com/nam.nodemy
+// Các bạn muốn học lập trình thì tham gia Nhóm zalo tự học lập trình nhé: https://zalo.me/g/yhdkef092
+const giftImage = 'hung.png';
+const base64 = '';
+const giftImageBase64 = "data:image/png;base64, " + base64;
