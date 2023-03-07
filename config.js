d2765759bfacec2979f48eca0a44007fe38fbf03
tuananh-05 <85606113+tuananh-05@users.noreply.github.com>
2023-03-07 09:14:19 +0700

    Update config.js

--- config.js
+++ config.js
@@ -7,6 +7,6 @@ const contentCard ='Bấm vào đây sẽ có điều bất ngờ nè !!';
 // phần dưới dành cho các bạn biết code, nếu muốn chỉnh ảnh đơn giản với base64
 // Mr-Nam http://facebook.com/nam.nodemy
 // Các bạn muốn học lập trình thì tham gia Nhóm zalo tự học lập trình nhé: https://zalo.me/g/yhdkef092
-const giftImage = 'rose.png';
+const giftImage = 'hung.png';
 const base64 = '';
 const giftImageBase64 = "data:image/png;base64, " + base64; 
